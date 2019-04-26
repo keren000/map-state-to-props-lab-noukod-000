78ea13eee2c2beb7e919b20e34177244b46fb0c7
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-26 17:59:18 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,13 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import UserInput from './UserInput'
 
+=======
+// import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import UserInput from './UserInput'
+>>>>>>> fe07c55e1630d74979f377a0675e9fabe1ad9fb2
 class Users extends Component {
 
   render() {
@@ -9,11 +15,19 @@ class Users extends Component {
       <div>
         <ul>
           // Users!
+<<<<<<< HEAD
         {this.props.users.map((user, index) =>
         <li key={index}>User: {user.username}, {user.hometown}</li>
         )}
         </ul>
         Total Users: {this.props.userCount}
+=======
+          {this.props.users.map((user, index) =>
+        <li key={index}>User: {user.username}, {user.hometown}</li>
+        )}
+        </ul>
+       Total Users: {this.props.userCount}
+>>>>>>> fe07c55e1630d74979f377a0675e9fabe1ad9fb2
       </div>
     )
   }
@@ -24,5 +38,12 @@ const mapStateToProps = (state) => {
   return { users: state.users, userCount: state.users.length }
 }
 
+<<<<<<< HEAD
+=======
+const mapStateToProps = (state) => {
+  return { users: state.users, userCount: state.users.length }
+}
+
+>>>>>>> fe07c55e1630d74979f377a0675e9fabe1ad9fb2
 // export default Users
 export default connect(mapStateToProps)(Users);
